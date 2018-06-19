340e9927ba02b2f278a5580d5249dd7274d12283
pursuitdan <32519457+pursuitdan@users.noreply.github.com>
2018-06-19 11:18:55 -0400

    Add files via upload

---
+++ fizzbuzz_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import os
+from fizzbuzz_c import *
+
+
+
+
+
+
+def test_three():
+    assert fizzbuzz(3) == "Fizz"
+
+def test_seven():
+    assert fizzbuzz(7) == "7"
